84528b891904cc2083615898eefa24a1c06f24da
Abbby1007 <Abigailloken25@gmail.com>
2026-06-05 20:09:24 +0900

    Added the rest of the robot controls

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,20 +1,27 @@
 extends Node2D
 
 @onready var robot: CharacterBody2D = $Robot
-#var roVelocity = $Robot.Vector2.ZERO
-
-var move_direction: Vector2 = Vector2.RIGHT
+var robotSpeed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-var move = false
+var moveLeft = false
+var moveRight = false
+var moveUp = false
+var moveDown = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if move:
-		robot.velocity = Vector2.LEFT * 400
-	
+	if moveLeft:
+		robot.velocity = Vector2.LEFT * robotSpeed
+	elif moveRight:
+		robot.velocity = Vector2.RIGHT * robotSpeed
+	elif moveUp:
+		robot.velocity = Vector2.UP * robotSpeed
+	elif moveDown:
+		robot.velocity = Vector2.DOWN * robotSpeed
 	else:
 		robot.velocity = Vector2.ZERO
 	robot.move_and_slide()
@@ -22,21 +29,37 @@ func _process(delta: float) -> void:
 
 #LEFT CONTROLS ----------------------
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Ttes")
 	if body.name == "Player":
-		move = true
-		
+		moveLeft = true
+
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 		if body.name == "Player":
-			move = false
-#-------------------------
+			moveLeft = false
 
 #RIGHT CONTROLS ----------------------------
+func _on_right_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		moveRight = true
 
-
-
+func _on_right_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		moveRight = false
+		
 #UP CONTROLS-----------------------
+func _on_up_arrow_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		moveUp = true
 
+func _on_up_arrow_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		moveUp = false
 
-#DOWN CONTROLS
+#DOWN CONTROLS---------------------
+func _on_down_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		moveDown = true
+
+func _on_down_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		moveDown = false
